a4af119497c5d67f029d254ac6dc0082983a99c2
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-01 23:19:48 +0500

    added stack.py file and classes

---
+++ recursion-and-stack/stack.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Stack: #it's same with Linked List
+    def __init__(self, head):
+        self.head = head
+    
+    def push(self, value):
+        next = self.head
+        self.head = Node(value)
+        self.head.next = next
+        
+    def pop(self):
+        value = self.head.value
+        self.head = self.head.next
+        return value
+    
+        
+    
