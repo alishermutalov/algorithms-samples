64b27e53c4e5bf0e0cc2a949985fecb62b7b15ff
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-01 23:23:38 +0500

    added isEmpty method

--- recursion-and-stack/stack.py
+++ recursion-and-stack/stack.py
@@ -18,5 +18,7 @@ class Stack: #it's same with Linked List
         self.head = self.head.next
         return value
     
-        
+    def isEmpty(self):
+        return self.head is None
+    
     
