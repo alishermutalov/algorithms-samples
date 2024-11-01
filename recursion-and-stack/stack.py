380bef49a951ca609864aead399ecc0bb27b1101
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-01 23:35:45 +0500

    edited something

--- recursion-and-stack/stack.py
+++ recursion-and-stack/stack.py
@@ -12,21 +12,26 @@ class Stack: #it's same with Linked List
         next = self.head
         self.head = Node(value)
         self.head.next = next
-        
+    
+    @property    
     def pop(self):
         value = self.head.value
         self.head = self.head.next
         return value
     
+    @property
     def is_empty(self):
         return self.head is None
     
+    @property
     def is_full(self):
         return False #It means There is no limit
 
+    @property
     def peek(self):
         return self.head.value
     
+    @property
     def print_all(self):
         item: Node = self.head
         while item:
