1c199dbf60f99b46edb38f2e0abec8b7e7408b5b
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-01 21:51:54 +0500

    added factorial calculator function

--- recursion-and-stack/recursion.py
+++ recursion-and-stack/recursion.py
@@ -1,3 +1,4 @@
+#example 1
 def count(n):
     print(n)
     if n<=1:
@@ -6,3 +7,12 @@ def count(n):
         count(n-1)
     
 count(10) # It will count from 10 to 1
+
+#example 2
+def factorial(x):
+    if x<=1:
+       return 1
+    else:
+        return x*factorial(x-1)
+    
+print(factorial(5))
