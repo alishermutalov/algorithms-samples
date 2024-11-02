16974c555a58089531cd0bfaae91e1b5ae2d67bf
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-02 15:12:56 +0500

    added example 2 (sum_list) function

--- sort-algorithms/quick_sort.py
+++ sort-algorithms/quick_sort.py
@@ -1,3 +1,4 @@
+# example 1
 def gcd(n:int,m:int)->int: #greatest common divisor
     if m == 0:
         return n
@@ -5,5 +6,12 @@ def gcd(n:int,m:int)->int: #greatest common divisor
         return gcd(m, n % m)
     
 print(gcd(45,27))
+
+#example 2
+def sum_list(list:list)->int | float:
+    if len(list)==0:
+        return 0
+    else:
+        return list.pop(-1) + sum_list(list)
     
-    
+print(sum_list([5,8,12,22]))
