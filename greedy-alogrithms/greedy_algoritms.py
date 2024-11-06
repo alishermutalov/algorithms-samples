048eb384b496375382d567da2bf30ec0939c6b61
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-06 16:48:58 +0500

    replaced code

--- greedy-alogrithms/greedy_algoritms.py
+++ greedy-alogrithms/greedy_algoritms.py
@@ -16,12 +16,3 @@ def findContentChildren(g:List[int], s:List[int])->int:
 
 print(findContentChildren([1,2,3],[1,1]))
 
-def countPairs(nums: List[int], target: int) -> int:
-    count = 0
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i]+nums[j]<target:
-                count+=1
-    return count
-
-print(countPairs([-6,2,5,-2,-7,-1,3],-2))
