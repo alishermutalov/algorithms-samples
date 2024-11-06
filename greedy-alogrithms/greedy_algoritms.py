c81854caf4efaa399c40bbfee0c353383de741d5
alishermutalov <96579968+alishermutalov@users.noreply.github.com>
2024-11-06 16:33:01 +0500

    Greedy algorithm

---
+++ greedy-alogrithms/greedy_algoritms.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def findContentChildren(g:List[int], s:List[int])->int:
+    g.sort()
+    s.sort()
+    counter = 0
+    i, j = 0, 0 
+    
+    while i < len(g) and j < len(s):
+        if s[j] >= g[i]:
+            counter += 1
+            i += 1  
+        j += 1
+
+    return counter
+
+print(findContentChildren([1,2,3],[1,1]))
